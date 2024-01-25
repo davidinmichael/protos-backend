ff185cd64de07718550ac5a24dea1bfd5a786868
davidinmichael <davidinmichael@gmail.com>
2024-01-25 13:51:34 +0100

    Swagger UI for API docs implemented.

--- location/urls.py
+++ location/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
 
     path("currencies/", CurrencyView.as_view()),
     path("countries/", CountryView.as_view()),
-    path("states/", StateView.as_view()),
-    path("cities/", CityView.as_view()),
+    # path("states/", StateView.as_view()),
+    # path("cities/", CityView.as_view()),
 
     path("states/<str:pk>/", CountryStateView.as_view()),
     path("cities/<str:pk>/", StateCityView.as_view()),
