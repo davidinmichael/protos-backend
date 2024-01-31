b93be2fb46bdb8bb567199abf69eadb285198de0
davidinmichael <davidinmichael@gmail.com>
2024-01-31 18:49:29 +0100

    Debug set to false for production.

--- protos/settings.py
+++ protos/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
