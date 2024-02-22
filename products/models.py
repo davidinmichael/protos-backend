c000f28a7563268a154088ac79e7f60d5e596b30
davidinmichael <davidinmichael@gmail.com>
2024-02-22 16:30:26 +0100

    Product and Product Category models created.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from accounts.models import BusinessAccount
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class BusinessProduct(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=30, decimal_places=4)
+    category = models.ForeignKey(ProductCategory, null=True, blank=True, on_delete=models.CASCADE)
+    business = models.ForeignKey(BusinessAccount, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='products/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.price} | {self.business.name}"
