a0cd993761fd6bc71fd1eed0c825b32b569c9210
davidinmichael <davidinmichael@gmail.com>
2024-02-22 17:09:47 +0100

    Product model image field added

--- products/models.py
+++ products/models.py
@@ -13,9 +13,11 @@ class BusinessProduct(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=30, decimal_places=4)
-    category = models.ForeignKey(ProductCategory, null=True, blank=True, on_delete=models.CASCADE)
+    category = models.ForeignKey(ProductCategory, null=True,
+                                 blank=True, on_delete=models.CASCADE)
     business = models.ForeignKey(BusinessAccount, on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to='products/', null=True, blank=True)
+    picture = models.ImageField(upload_to='products/', null=True,
+                                blank=True, default="default-product.jpg")
 
     def __str__(self):
         return f"{self.name} - {self.price} | {self.business.name}"
